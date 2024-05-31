5ef2bb521797f06a075539dcb70b23e12c4b0fa5
Alex <alexedwa123@gmail.com>
2024-05-31 23:13:35 +0100

    Made edits to the style of the buttons.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -13,16 +13,19 @@ tictactoe::~tictactoe()
 {
     delete ui;
 }
-// -1 = not chosen, 1 = O, 0 = x
-int board[3][3] = {
-    {-1, -1, -1},       //a3, b3, c3
-    {-1, -1, -1},       //a2, b2, c2
-    {-1, -1, -1}        //a1, b1, c1
-};
 
-int turnCount = 1;
+void tictactoe::checkWin(){
+    if (turnCount % 2 == 1){        // changing to naughts
+        ui->X_Turn->setStyleSheet("background-color: black;\ncolor: black;\nfont-size: 50px;");
+        ui->O_Turn->setStyleSheet("background-color: blue;\ncolor: black;\nfont-size: 50px;");
 
-int checkWin(){
+    }
+    else if (turnCount % 2 == 0){       //changing to crosses
+        ui->X_Turn->setStyleSheet("background-color: red;\ncolor: black;\nfont-size: 50px;");
+        ui->O_Turn->setStyleSheet("background-color: black;\ncolor: black\nfont-size: 50px;");
+    }
+
+    /*
     //diagonal win
     if ( (board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] == board[1][1] && board[1][1] == board[2][0]) ){
         if (board[1][1] != -1) return turnCount % 2;     // x = 1, o = 0 (very confusing i know)
@@ -39,187 +42,179 @@ int checkWin(){
     else if ( (board[0][0] == board[1][0] && board[1][0] == board[2][0]) || (board[0][1] == board[1][1] && board[1][1] == board[2][1]) || (board[0][2] == board[1][2] && board[1][2] == board[2][2]) ){
         if (board[0][0] != -1) return turnCount % 2;
         else if (board[0][1] != -1) return turnCount % 2;
-        else if (board[0][2] != -1) return turnCount % 2;     }
+        else if (board[0][2] != -1) return turnCount % 2;
+    }
 
     else return -1;
+    */
 }
 
 void tictactoe::on_a1_Button_clicked()
 {
-    if (board[2][0] == -1){
-        if (turnCount % 2 == 0){
-            board[2][0] = 1;
-        }else{
-            board[2][0] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[2][0] == 0 || board[2][0] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->a1_Button->setText("O");
+        board[2][0] = 1;
+    }else{
+        ui->a1_Button->setText("X");
+        board[2][0] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_a2_Button_clicked()
 {
-    if (board[1][0] == -1){
-        if (turnCount % 2 == 0){
-            board[1][0] = 1;
-        }else{
-            board[1][0] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[1][0] == 0 || board[1][0] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->a2_Button->setText("O");
+        board[1][0] = 1;
+    }else{
+        ui->a2_Button->setText("X");
+        board[1][0] = 0;
+    }
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_a3_Button_clicked()
 {
-    if (board[0][0] == -1){
-        if (turnCount % 2 == 0){
-            board[0][0] = 1;
-        }else{
-            board[0][0] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[0][0] == 0 || board[0][0] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->a3_Button->setText("O");
+        board[0][0] = 1;
+    }else{
+        ui->a3_Button->setText("X");
+        board[0][0] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_b1_Button_clicked()
 {
-    if (board[2][1] == -1){
-        if (turnCount % 2 == 0){
-            board[2][1] = 1;
-        }else{
-            board[2][1] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[2][1] == 0 || board[2][1] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->b1_Button->setText("O");
+        board[2][1] = 1;
+    }else{
+        ui->b1_Button->setText("X");
+        board[2][1] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_b2_Button_clicked()
 {
-    if (board[1][1] == -1){
-        if (turnCount % 2 == 0){
-            board[1][1] = 1;
-        }else{
-            board[1][1] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[1][1] == 0 || board[1][1] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->b2_Button->setText("O");
+        board[1][1] = 1;
+    }else{
+        ui->b2_Button->setText("X");
+        board[1][1] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_b3_Button_clicked()
 {
-    if (board[0][1] == -1){
-        if (turnCount % 2 == 0){
-            board[0][1] = 1;
-        }else{
-            board[0][1] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[0][1] == 0 || board[0][1] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->b3_Button->setText("O");
+        board[0][1] = 1;
+    }else{
+        ui->b3_Button->setText("X");
+        board[0][1] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_c1_Button_clicked()
 {
-    if (board[2][2] == -1){
-        if (turnCount % 2 == 0){
-            board[2][2] = 1;
-        }else{
-            board[2][2] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[2][2] == 0 || board[2][2] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->c1_Button->setText("O");
+        board[2][2] = 1;
+    }else{
+        ui->c1_Button->setText("X");
+        board[2][2] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_c2_Button_clicked()
 {
-    if (board[1][2] == -1){
-        if (turnCount % 2 == 0){
-            board[1][2] = 1;
-        }else{
-            board[1][2] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[1][2] == 0 || board[1][2] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->c2_Button->setText("O");
+        board[1][2] = 1;
+    }else{
+        ui->c2_Button->setText("X");
+        board[1][2] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
 
 void tictactoe::on_c3_Button_clicked()
 {
-    if (board[0][2] == -1){
-        if (turnCount % 2 == 0){
-            board[0][2] = 1;
-        }else{
-            board[0][2] = 0;
-        }
-
-        if (checkWin() == 0){
-            QMessageBox::about(this, "Winner!", "Naughts is the winner!");
-        }
-        else if (checkWin() == 1){
-            QMessageBox::about(this, "Winner!", "Crosses is the winner!");
-        }
-        turnCount++;
+    if (board[0][2] == 0 || board[0][2] == 1){
+        return;
     }
+
+    if (turnCount % 2 == 0){
+        ui->c3_Button->setText("O");
+        board[0][2] = 1;
+    }else{
+        ui->c3_Button->setText("X");
+        board[0][2] = 0;
+    }
+
+    checkWin();
+    turnCount++;
 }
 
